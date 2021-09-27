da58b97f2adc8f99be8554114f6e831d42407abd
raul55lp <49528540+raul55lp@users.noreply.github.com>
2021-09-28 01:41:16 +0200

    Add project files.

---
+++ Amazon.APIi18n/Models/ProductName.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Amazon.APIi18n.Models
+{
+    public class ProductName
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Amazon.APIi18n/Repositories/EnglishRepository.cs
@@ -0,0 +1,44 @@
+﻿using Amazon.APIi18n.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Amazon.APIi18n.Repositories
+{
+    public class EnglishRepository
+    {
+        private static string _path = @"~\..\BBDD\ProductsEnglish.json";
+        public List<ProductName> GetAllNames()
+        {
+            return ReadBBDD();
+        }
+
+        public ProductName GetProductName(int id)
+        {
+            return ReadBBDD().FirstOrDefault(p => p.Id == id);
+        }
+
+        public bool UpdateProductName(ProductName product)
+        {
+            List<ProductName> products = ReadBBDD();
+            int i = products.FindIndex(p => p.Id == product.Id);
+            if (i != -1)
+            {
+                products[i] = product;
+                var json = JsonConvert.SerializeObject(products);
+                File.WriteAllText(_path, json);
+                return true;
+            }
+            return false;
+        }
+
+        private List<ProductName> ReadBBDD()
+        {
+            string json = File.ReadAllText(_path);
+            return JsonConvert.DeserializeObject<List<ProductName>>(json);
+        }
+    }
+}
